7c910f38265f554d10b4bc836b1aa6df9da61d9e
unknown <axel.theule@gmail.com>
2025-04-05 00:33:24 +0200

    flame library wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -6,21 +7,32 @@ void main() {
 }
 
 class ZooTycoonGame extends FlameGame {
+  late SpriteComponent background;
+
   @override
   Future<void> onLoad() async {
-    // Ici, vous pouvez charger vos assets (images, sons, etc.)
-    // Par exemple : await images.load('zoo_background.png');
+    // Charger l'image de fond depuis le dossier assets
+    final bgImage = await images.load('background.png');
+
+    // Créer un composant sprite avec l'image chargée
+    background = SpriteComponent(
+      sprite: Sprite(bgImage),
+      size: size, // 'size' correspond aux dimensions du jeu (souvent la taille de l'écran)
+    );
+
+    // Ajouter le composant à la scène du jeu
+    add(background);
   }
 
   @override
   void update(double dt) {
-    // Logique de mise à jour du jeu (gestion des états, interactions, etc.)
+    // Ici vous pouvez mettre à jour la logique du jeu
     super.update(dt);
   }
 
   @override
   void render(Canvas canvas) {
-    // Code de rendu : dessiner le fond, les animaux, les bâtiments, etc.
+    // La méthode render est automatiquement gérée par Flame pour afficher vos composants
     super.render(canvas);
   }
 }
